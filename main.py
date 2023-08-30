af574fc4addc86accbee149dd3ad4be6a1047404
koleq(WORK-D) <o.kurz@atlas.cz>
2023-08-30 10:22:09 +0200

    bulk commits suck, why am I doing this

--- main.py
+++ main.py
@@ -6,65 +6,83 @@ app = Flask(__name__)
 
 status = "True"
 #text = "Cigarettes and tiny liquor bottles,\nJust what you'd expect inside her new Balenciaga.\nWild romance turned dreams into an empire.\nSelf-made success now she rolls with Rockefellers."
-#text = "I am sitting\nIn the morning\nAt the diner\nOn the corner"
-text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis volutpat condimentum. In posuere metus mi, volutpat sagittis quam interdum quis. Donec vulputate ornare nunc, et fringilla velit eleifend malesuada. Duis non lorem mattis, elementum dolor quis, tristique risus. Mauris tempor lacus massa, a dapibus lorem eleifend sit amet. Donec lacinia venenatis libero, vel dictum elit euismod nec. Aliquam tristique."
+text = "I am sitting\nIn the morning\nAt the diner\nOn the corner"
+#ext = "I am sitting in the morning at the diner on the corner"
+#text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis volutpat condimentum. In posuere metus mi, volutpat sagittis quam interdum quis. Donec vulputate ornare nunc, et fringilla velit eleifend malesuada. Duis non lorem mattis, elementum dolor quis, tristique risus. Mauris tempor lacus massa, a dapibus lorem eleifend sit amet. Donec lacinia venenatis libero, vel dictum elit euismod nec. Aliquam tristique."
 
+#region index
 @app.route("/")
 def index():
     logic()
     return render_template("index.html", text=Markup(text.replace("\n", "<br>")), status=status)
+#endregion
 
+#region settings
 @app.route("/settings")
 def settings():
     logic()
     return render_template("settings.html")
+#endregion
 
 index_data = ""
 settings_data = "nothing"
 
 started = False
+ended = False
 starttime = 0
 endtime = 0
 tooktime = 0
-last_correct = 0
+last_correct_length = 0
 
+#region index_input
 @app.route("/index_input", methods=["POST"])
 def index_input():
     global index_data
     index_data = request.data.decode("utf-8")
-    length, rettext, tooktime, settings_data = logic()
-    return jsonify(status=status, number=length, text=Markup(rettext.replace("\n", "<br>")), took=tooktime, stopat=settings_data)
+    length, rettext, tooktime, settings_data, rows = logic()
+    return jsonify(status=status, number=length, text=Markup(rettext.replace("\n", "<br>")), took=tooktime, stopat=settings_data, rows=rows)
+#endregion
 
+#region settings_input
 @app.route("/settings_input", methods=["POST"])
 def settings_input():
     global settings_data
     settings_data = request.data.decode("utf-8")
     logic()
-    #print("settings_input="+settings_data)
     return settings_data
+#endregion
 
+#region logic
 def logic():
     global status
     global started
+    global ended
     global starttime
     global endtime
     global tooktime
+    global last_correct_length
     global last_correct
 
     global index_data
     global settings_data
 
     length = len(index_data)
+    rows = len(text.split("\n"))+1
 
-    print("logic="+settings_data)
-
+    #region Time and WPM caltulation
     if length == 1:
         started = True
         starttime = time()
+
+    if length == len(text)-1:
+        ended = True
     
     if length == len(text):
-        if started:
+        if status == "True" and started == False and ended == False:
+            tooktime = "no pasting cheater"
+        elif started and ended: # prevents pasting the text
             started = False
+            ended = False
             endtime = time()
             tooktime = endtime - starttime
 
@@ -78,20 +96,22 @@ def logic():
                 tooktime = f"test took: {int(tooktime/60)}min {round(tooktime%60, 2)}s {round(wpm, 2)}rWPM {round(wpm5, 2)}avgwWPM"
     else:
         tooktime = "finish a test to get a time"
+    #endregion
 
-    print(length, last_correct)
-
+    #region text coloring
     if text[:length] == index_data:
         status = "True"
         rettext = f"<span id=\"correct\">{text[:length]}</span>{text[length:]}"
-        last_correct = length
+        last_correct_length = length
     else:
         status = "False"
-        wrong_text = text[last_correct:length].replace(' ', '·')
+        wrong_text = text[last_correct_length:length].replace(' ', '·')
         wrong_text = wrong_text.replace('\n', "↵\n")
-        rettext = f"<span id=\"correct\">{text[:last_correct]}</span><span id=\"wrong\">{wrong_text}</span>{text[length:]}"
+        rettext = f"<span id=\"correct\">{text[:last_correct_length]}</span><span id=\"wrong\">{wrong_text}</span>{text[length:]}"
+    #endregion
 
-    return length, rettext, tooktime, settings_data
+    return length, rettext, tooktime, settings_data, rows
+#endregion
 
 if __name__ == '__main__':
     app.run()
