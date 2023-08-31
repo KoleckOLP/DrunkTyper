f886eee1cc5d29d5aa7851ba7ce4bdce38be18fd
koleq(WORK-D) <o.kurz@atlas.cz>
2023-08-31 10:03:31 +0200

    Implement 50 words random

--- main.py
+++ main.py
@@ -1,18 +1,35 @@
 from flask import Flask, request, render_template, jsonify
 from markupsafe import Markup
 from time import time
+import random
 
 app = Flask(__name__)
 
 status = "True"
 #text = "Cigarettes and tiny liquor bottles,\nJust what you'd expect inside her new Balenciaga.\nWild romance turned dreams into an empire.\nSelf-made success now she rolls with Rockefellers."
-text = "I am sitting\nIn the morning\nAt the diner\nOn the corner"
+#text = "I am sitting\nIn the morning\nAt the diner\nOn the corner"
 #ext = "I am sitting in the morning at the diner on the corner"
 #text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque mollis volutpat condimentum. In posuere metus mi, volutpat sagittis quam interdum quis. Donec vulputate ornare nunc, et fringilla velit eleifend malesuada. Duis non lorem mattis, elementum dolor quis, tristique risus. Mauris tempor lacus massa, a dapibus lorem eleifend sit amet. Donec lacinia venenatis libero, vel dictum elit euismod nec. Aliquam tristique."
+
+text = ""
+
+with open('english200.txt', 'r') as file:
+    words = file.read().splitlines()
+
+for i in range(49):
+    text = text + random.choice(words) + " "
+    if i == 48:
+        text = text + random.choice(words)
 
 #region index
 @app.route("/")
 def index():
+    global text
+    text = ""
+    for i in range(49):
+        text = text + random.choice(words) + " "
+    if i == 48:
+        text = text + random.choice(words)
     logic()
     return render_template("index.html", text=Markup(text.replace("\n", "<br>")), status=status)
 #endregion
@@ -61,7 +78,6 @@ def logic():
     global endtime
     global tooktime
     global last_correct_length
-    global last_correct
 
     global index_data
     global settings_data
@@ -91,9 +107,9 @@ def logic():
             wpm5 = ((len(text) / 5) / tooktime ) * 60
 
             if tooktime < 60:
-                tooktime = f"test took: {round(tooktime, 2)}s {round(wpm, 2)}rWPM {round(wpm5, 2)}aWPM"
+                tooktime = f"test took: {round(tooktime, 2)}s {round(wpm, 2)} rWPM {round(wpm5, 2)} aWPM"
             else:
-                tooktime = f"test took: {int(tooktime/60)}min {round(tooktime%60, 2)}s {round(wpm, 2)}rWPM {round(wpm5, 2)}avgwWPM"
+                tooktime = f"test took: {int(tooktime/60)}min {round(tooktime%60, 2)}s {round(wpm, 2)} rWPM {round(wpm5, 2)} avgwWPM"
     else:
         tooktime = "finish a test to get a time"
     #endregion
